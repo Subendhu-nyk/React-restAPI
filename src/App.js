fa9c7f23f7a3d98bcaf20c08653cedc73c1dc805
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-11-26 16:51:17 +0530

    fetch movie on pageload(t-9)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect,useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 import load from './components/load.gif'
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies,setMovies]=useState([]);
   const [isLoading,setIsLoading]=useState(false)
+  const [error,setError]=useState(null)
+ 
 
   // function fetchMoviesHandler(){
     // through promises
@@ -27,10 +30,16 @@ function App() {
     // })
   // }
 
-   async function fetchMoviesHandler(){    
+   const fetchMoviesHandler =  useCallback(async ()=> {    
     setIsLoading(true)
-   const response= await fetch('https://swapi.dev/api/films/')
-   const data= await response.json();   
+    setError(null)
+    try{
+      const response= await fetch('https://swapi.dev/api/films/')
+   const data= await response.json();  
+      
+      if(!response.ok){
+        throw new Error('something went wrong')
+      }
       const transformedMovies= data.results.map(movieData=>{
         return {
           id:movieData.episode_id,
@@ -40,17 +49,34 @@ function App() {
         };
       })
       setMovies(transformedMovies)   
-      setIsLoading(false)
+     
+    }
+    catch(error){
+      setError(error.message)
+    }
+    setIsLoading(false)
+  },[]);
+
+   useEffect(()=>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
+
+  function addMovieHandler(movie){
+    console.log(movie)
   }
-  
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}/>
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-       {!isLoading && movies.length===0 && <p>Found no movies</p>}
+       {!isLoading && movies.length===0 && !error && <p>Found no movies</p>}
+       {!isLoading && error && <p>{error}</p>}
        {isLoading && <img src={load} alt='loading'/>}
       </section>
     </React.Fragment>
